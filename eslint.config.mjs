3f08f790557d5441ff485fd4fb1a981c36c69440
sergeavila7 <serge_avila7@yahoo.com>
2026-05-13 16:35:42 -0600

    fix(eslint): fix ESLint flat config setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs', 'dist'],
   },
 
-  eslint.configs.recommended,
+  js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
 
@@ -26,7 +31,6 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-
       '@typescript-eslint/no-unsafe-call': 'warn',
 
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
