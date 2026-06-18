0890b84daaf322e8d936850efb35606c7cb0f48c
sergeavila7 <serge_avila7@yahoo.com>
2026-06-18 16:40:18 -0600

    feat(clients): add cache support

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { AuthModule } from '../auth/auth.module';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CacheModule],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
