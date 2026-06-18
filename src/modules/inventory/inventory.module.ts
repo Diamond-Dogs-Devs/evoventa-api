980f333b8d0893920836431cf0c4314f8e26fdc7
sergeavila7 <serge_avila7@yahoo.com>
2026-06-18 16:40:26 -0600

    feat(inventory): add cache support

--- src/modules/inventory/inventory.module.ts
+++ src/modules/inventory/inventory.module.ts
@@ -3,9 +3,10 @@ import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { ProductsModule } from '../products/products.module';
 import { AuthModule } from '../auth/auth.module';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [AuthModule, ProductsModule],
+  imports: [AuthModule, ProductsModule, CacheModule],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
